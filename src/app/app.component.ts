a562bd13e8ccdc68cf5de8cf0798802604febfa4
Scott Isitt <isitts@hotmail.com>
2022-08-02 19:01:38 -0700

    Import HttpClientModule and set up simple fetch request in app TS component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-fetch-api-practice';
+  title = 'Angular Fetch API Practice';
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.getApi();
+  }
+
+  getApi() {
+    this.http.get('https://api.ebird.org/v2/ref/taxonomy/ebird?species=hottea1&version=2022').subscribe(
+      {
+        next: response => response,
+        error: error => console.log(error)
+      }
+    )
+  }
+
 }
